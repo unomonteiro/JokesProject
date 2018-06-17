83fd9655e6c47effb7b57f137492f8237c299d93
Carlos Monteiro <offbyte@gmail.com>
2018-06-17 13:14:36 +0100

    Improve AsyncTask Test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -1,17 +1,39 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.hamcrest.core.IsNot.not;
+import java.util.concurrent.CountDownLatch;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
-import static org.junit.Assert.assertThat;
 
+
+@RunWith(AndroidJUnit4.class)
 public class EndpointsAsyncTaskTest {
 
     @Test
-    public void EndpointsAsyncTask_returnsNonEmptyString() throws Exception {
-        String jokeString = new EndpointsAsyncTask().execute().get();
-        assertThat(jokeString, not(isEmptyOrNullString()));
+    public void EndpointsAsyncTask_returnsNonEmptyString() throws InterruptedException {
+//        String jokeString = new EndpointsAsyncTask().execute().get();
+//        assertThat(jokeString, not(isEmptyOrNullString()));
+        final CountDownLatch count = new CountDownLatch(1);
+        EndpointsAsyncTask jokeTask = new EndpointsAsyncTask(){
+            @Override
+            protected void onPostExecute(String result) {
+                assertNotNull(result);
+                //check that the result is not an error message from the asyncTask
+                assertTrue(!result.contains("Error"));
+                assertThat(result, not(isEmptyOrNullString()));
+                count.countDown();
+            }
+        };
+        jokeTask.execute();
+        count.await();
     }
 
 }
